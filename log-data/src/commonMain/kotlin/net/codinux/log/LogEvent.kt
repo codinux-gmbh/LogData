a4b63578ae216037d5d919058573590c58605d8c
dankito <git@dankito.net>
2025-05-21 02:57:35 +0200

    Added default values for optional fields

--- log-data/src/commonMain/kotlin/net/codinux/log/LogEvent.kt
+++ log-data/src/commonMain/kotlin/net/codinux/log/LogEvent.kt
@@ -4,8 +4,8 @@ open class LogEvent(
     open val level: LogLevel,
     open val message: String,
     open val loggerName: String,
-    open val threadName: String?,
-    open val exception: Throwable?
+    open val threadName: String? = null,
+    open val exception: Throwable? = null,
 ) {
     override fun toString() = "$level [$loggerName] $message"
 }
