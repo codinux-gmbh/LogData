18db7895b0688d2dbb13c3846305bbc870fdbc25
dankito <git@dankito.net>
2025-05-28 18:55:04 +0200

    Added equals() and hashCode() implementations

--- log-data/src/commonMain/kotlin/net/codinux/log/LogEvent.kt
+++ log-data/src/commonMain/kotlin/net/codinux/log/LogEvent.kt
@@ -16,5 +16,31 @@ open class LogEvent(
             : this(null, level, message, loggerName, threadName, exception)
 
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is LogEvent) return false
+
+        if (timestamp != other.timestamp) return false
+        if (level != other.level) return false
+        if (message != other.message) return false
+        if (loggerName != other.loggerName) return false
+        if (threadName != other.threadName) return false
+        if (exception != other.exception) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = timestamp?.hashCode() ?: 0
+        result = 31 * result + level.hashCode()
+        result = 31 * result + message.hashCode()
+        result = 31 * result + loggerName.hashCode()
+        result = 31 * result + (threadName?.hashCode() ?: 0)
+        result = 31 * result + (exception?.hashCode() ?: 0)
+        return result
+    }
+
+
     override fun toString() = "$level [$loggerName] $message"
+
 }
